f14b7a1489d00f22cf9134c509041c0fb188d10f
Arpit Anand <arpitssanand@gmail.com>
2021-07-26 23:04:07 +0530

    Add Resume Data API

---
+++ resumeData.ts
@@ -0,0 +1,111 @@
+const resumeData = {
+  main: {
+    name: "Arpit Anand",
+    occupation: "Full - Stack Web Developer",
+    description: "I eat, sleep and breathe Code.",
+    image: "",
+    bio: "Hi there! I am self taught web developer with a vast array of knowledge in many different front end and back end languages, responsive frameworks, databases, and best code practices. My objective is simply to be the best web developer that I can be and to contribute to the technology industry all that I know and can do. I am dedicated to perfecting my craft by learning from more seasoned developers, remaining humble, and continuously making strides to learn all that I can about development. While a student at Veer Bahadur Singh Purvanchal University, I studied Mechanical Engineering, but I believe that my understanding of problem solving and complex algorithms are also skills that have and will continue to contribute to my overall success as a developer.",
+    contactmessage: "Get in touch with me together we can make changes.",
+    email: "arpitssanand@gmail.com",
+    phone: "+91 6394236990",
+    address: {
+      street: "172, Vanvihar Road, Wajidpur",
+      city: "Jaunpur",
+      state: "Uttar Pradesh",
+      country: "India",
+      zip: "222002",
+    },
+    website: "www.thearpitanand.com",
+    resumedownload: "www.thearpitanand.com/resume",
+    social: [
+      {
+        name: "Facebook",
+        url: "",
+        className: "fa fa-facebook",
+      },
+      {
+        name: "LinkedIn",
+        url: "",
+        className: "fa fa-linkedin",
+      },
+      {
+        name: "Instagram",
+        url: "",
+        className: "fa fa-instagram",
+      },
+      {
+        name: "Github",
+        url: "",
+        className: "fa fa-github",
+      },
+    ],
+  },
+  resume: {
+    skillmessage: "My Programming Language Proficiency",
+    education: [
+      {
+        school: "Veer Bahadur Singh Purvanchal University",
+        degree: "Bachelor of Technology, Mechanical Engineering",
+        graduated: "November 2020",
+        description: "Graduated with --% Aggregate",
+      },
+    ],
+    work: [
+      {
+        company: "",
+        title: "",
+        years: "May 2019 - Present",
+        description: "",
+      },
+    ],
+    skills: [
+      {
+        name: "ReactJs",
+        level: "100%",
+      },
+      {
+        name: "Python",
+        level: "40%",
+      },
+      {
+        name: "Git",
+        level: "70%",
+      },
+      {
+        name: "JavaScript",
+        level: "100%",
+      },
+      {
+        name: "Web Development",
+        level: "99%",
+      },
+      {
+        name: "MongoDB",
+        level: "89%",
+      },
+      {
+        name: "Node",
+        level: "94%",
+      },
+    ],
+  },
+  portfolio: {
+    projects: [
+      {
+        title: "",
+        category: "",
+        image: "",
+        url: "",
+      },
+    ],
+  },
+  testimonials: {
+    testimonials: [
+      {
+        text: "",
+        user: "",
+      },
+    ],
+  },
+};
+export default resumeData;
