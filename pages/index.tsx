a64c057c502f8c0422cc70dc813cf363e36329b8
Arpit Anand <arpitssanand@gmail.com>
2021-07-29 21:40:34 +0530

    Add Biography

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,6 +6,7 @@ import Navbar from "../components/Navbar/Navbar";
 import About from "../components/About/About";
 import HeadContent from "../components/HeadContent/HeadContent";
 import Footer from "../components/Footer/Footer";
+import Biography from "../components/Biography/Biography";
 
 const Home = () => {
   const [main, setMain] = useState({});
@@ -31,6 +32,7 @@ const Home = () => {
       <div>
         <Navbar />
         <About main={main} />
+        <Biography main={main} />
         <Footer />
       </div>
     </React.Fragment>
--- resumeData.ts
+++ resumeData.ts
@@ -1,8 +1,10 @@
 const resumeData = {
   main: {
     name: "Arpit Anand",
+    profilepic:"/assets/images/profilepic.png",
     description: "I eat, sleep and breathe Code.",
-    bio: "Hi there! I am self taught web developer with a vast array of knowledge in many different front end and back end languages, responsive frameworks, databases, and best code practices. My objective is simply to be the best web developer that I can be and to contribute to the technology industry all that I know and can do. I am dedicated to perfecting my craft by learning from more seasoned developers, remaining humble, and continuously making strides to learn all that I can about development. While a student at Veer Bahadur Singh Purvanchal University, I studied Mechanical Engineering, but I believe that my understanding of problem solving and complex algorithms are also skills that have and will continue to contribute to my overall success as a developer.",
+    bio: "Hi there! I am self taught web developer with a vast array of knowledge in many different front end and back end languages, responsive frameworks, databases, and best code practices. ",
+    fullbio:"Hi there! I am self taught web developer with a vast array of knowledge in many different front end and back end languages, responsive frameworks, databases, and best code practices. My objective is simply to be the best web developer that I can be and to contribute to the technology industry all that I know and can do. I am dedicated to perfecting my craft by learning from more seasoned developers, remaining humble, and continuously making strides to learn all that I can about development. While a student at Veer Bahadur Singh Purvanchal University, I studied Mechanical Engineering, but I believe that my understanding of problem solving and complex algorithms are also skills that have and will continue to contribute to my overall success as a developer.",
     contactmessage: "Get in touch with me together we can make changes.",
     email: "arpitssanand@gmail.com",
     mobilenumber:"+91 6394236990",
