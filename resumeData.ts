b4c73f8a825106884942e9eec1e409a485dca4f8
Arpit Anand <arpitssanand@gmail.com>
2021-08-18 19:48:30 +0530

    Update resume data

--- resumeData.ts
+++ resumeData.ts
@@ -8,9 +8,8 @@ const resumeData = {
       "Hi there! I am self taught web developer with a vast array of knowledge in many different front end and back end languages, responsive frameworks, databases, and best code practices. My objective is simply to be the best web developer that I can be and to contribute to the technology industry all that I know and can do. I am dedicated to perfecting my craft by learning from more seasoned developers, remaining humble, and continuously making strides to learn all that I can about development. While a student at Veer Bahadur Singh Purvanchal University, I studied Mechanical Engineering, but I believe that my understanding of problem solving and complex algorithms are also skills that have and will continue to contribute to my overall success as a developer.",
     contactmessage: "Get in touch with me together we can make changes.",
     email: "arpitssanand@gmail.com",
-    mobilenumber: "+91 6394236990",
     address: {
-      street: "172, Vanvihar Road, Wajidpur",
+      street: "Wajidpur",
       city: "Jaunpur",
       state: "Uttar Pradesh",
       country: "India",
